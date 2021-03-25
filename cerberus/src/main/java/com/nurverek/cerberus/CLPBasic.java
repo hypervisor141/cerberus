92130d4798cab6a153476b6f7ea754ac894dd15c
Nurverek <nurverek@gmail.com>
2021-03-25 16:24:24 +0430

    Almost done with positions

--- cerberus/src/main/java/com/nurverek/cerberus/CLPBasic.java
+++ cerberus/src/main/java/com/nurverek/cerberus/CLPBasic.java
@@ -1,6 +1,12 @@
 package com.nurverek.cerberus;
 
+import android.opengl.GLES32;
+
 import com.nurverek.firestorm.FSBrightness;
+import com.nurverek.firestorm.FSConfig;
+import com.nurverek.firestorm.FSConfigDynamic;
+import com.nurverek.firestorm.FSConfigGroup;
+import com.nurverek.firestorm.FSControl;
 import com.nurverek.firestorm.FSG;
 import com.nurverek.firestorm.FSGamma;
 import com.nurverek.firestorm.FSLight;
@@ -28,6 +34,7 @@ public class CLPBasic extends FSP{
 
     public CLPBasic(int meshcapacity, int debugmode){
         super(10, meshcapacity, debugmode);
+        
         settings = new Settings();
     }
 
@@ -49,6 +56,26 @@ public class CLPBasic extends FSP{
 
     public void setTypeNormals(int type){
         settings.type_normals = type;
+    }
+
+    public void setUBOSegmentsModel(int type){
+        settings.modelsegments = type;
+    }
+
+    public void setUBOSegmentsPosition(int type){
+        settings.positionsegments = type;
+    }
+
+    public void setUBOSegmentsColor(int type){
+        settings.colorsegments = type;
+    }
+
+    public void setUBOSegmentsTexCoords(int type){
+        settings.texcoordsegments = type;
+    }
+
+    public void setUBOSegmentsNormals(int type){
+        settings.normalssegments = type;
     }
 
     public void setTexture(boolean enabled){
@@ -93,8 +120,74 @@ public class CLPBasic extends FSP{
     }
 
     @Override
-    public void customize(VLListType<FSShader> shaders, int debug){
-        
+    public void customize(VLListType<FSMesh> meshes, VLListType<FSShader> shaders, int debug){
+        FSShader vshader = new FSShader(GLES32.GL_VERTEX_SHADER);
+        FSShader fshader = new FSShader(GLES32.GL_FRAGMENT_SHADER);
+
+        shaders.add(vshader);
+        shaders.add(fshader);
+
+        FSConfigGroup predraw = new FSConfigGroup(FSConfig.MODE_ALWAYS,10, 10);
+        FSConfigGroup draw = new FSConfigGroup(FSConfig.MODE_ALWAYS,10, 10);
+        FSConfigGroup postdraw = new FSConfigGroup(FSConfig.MODE_ALWAYS,10, 10);
+
+        setupconfig = predraw;
+        meshconfig = draw;
+        postdrawconfig = postdraw;
+
+        if(settings.type_position == TYPE_ATTRIBUTE){
+            FSConfig positions = new FSP.AttribPointer(FSConfig.MODE_ALWAYS, FSG.ELEMENT_POSITION, 0);
+            FSConfig enableposition = new FSP.AttribEnable(FSConfig.MODE_ALWAYS, positions.location());
+            FSConfig disableposition = new FSP.AttribDisable(FSConfig.MODE_ALWAYS, positions.location());
+
+            registerAttributeLocation(vshader, positions);
+
+            predraw.configs.add(enableposition);
+            draw.configs.add(positions);
+            postdraw.configs.add(disableposition);
+
+            vshader.addAttribute(positions.location(), "vec4", "position");
+
+        }else if(settings.type_position == TYPE_UNIFORM){
+            int count = meshes.get(0).instance(0).positions().size() / FSG.UNIT_SIZE_POSITION;
+            FSConfig positions = new FSP.Uniform4fve(FSConfig.MODE_ONETIME, 0, FSG.ELEMENT_POSITION, 0, count);
+
+            registerUniformArrayLocation(vshader, count, positions);
+            draw.configs.add(positions);
+            vshader.addUniform(positions.location(), "vec4", "position");
+
+        }else if(settings.type_position == TYPE_UBO){
+            int size = meshes.size();
+            int size2 = 0;
+            int size3 = 0;
+            int max = 0;
+            int maxmeshindex = 0;
+            int maxinstanceindex = 0;
+
+            FSMesh mesh;
+
+            for(int i = 0; i < size; i++){
+                mesh = meshes.get(i);
+                size2 = mesh.size();
+
+                for(int i2 = 0; i2 < size2; i2++){
+                    size3 = mesh.instance(i2).positions().size();
+
+                    if(max < size3){
+                        max = size3;
+                        maxmeshindex = i;
+                        maxinstanceindex = i2;
+                    }
+                }
+            }
+
+            segmentation needed
+
+            FSConfig positions = new FSP.UniformBlockElement(FSConfig.MODE_ALWAYS, FSG.ELEMENT_POSITION, "POSITIONS", 0);
+
+            draw.configs.add(positions);
+            vshader.addUniformBlock("std140", "POSITIONS", "vec4 positions[" + meshes.get(maxmeshindex).instance(maxinstanceindex).vertexSize() + "];");
+        }
     }
 
     @Override
@@ -126,6 +219,12 @@ public class CLPBasic extends FSP{
         public boolean lighting;
         public boolean shadows;
 
+        public int modelsegments;
+        public int positionsegments;
+        public int colorsegments;
+        public int texcoordsegments;
+        public int normalssegments;
+
         private Settings(){
             type_model = TYPE_DISABLED;
             type_position = TYPE_DISABLED;
@@ -142,6 +241,12 @@ public class CLPBasic extends FSP{
 
             shadows = false;
             lighting = false;
+
+            modelsegments = 1;
+            positionsegments = 1;
+            colorsegments = 1;
+            texcoordsegments = 1;
+            normalssegments = 1;
         }
     }
 }
